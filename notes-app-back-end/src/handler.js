ef2dacf2ee7816fe69c363473328842de06bd3cc
vendraaaAjaaa <vendrafausta88@gmail.com>
2025-12-29 22:49:12 +0700

    update menampilkan catatan

--- notes-app-back-end/src/handler.js
+++ notes-app-back-end/src/handler.js
@@ -35,4 +35,34 @@ const addNoteHandler = (request, h) => {
   response.code(500);
   return response;
 };
-module.exports = { addNoteHandler };
+
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,10 +1,21 @@
-const { addNoteHandler } = require('./handler');
+const { method } = require('lodash');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 
