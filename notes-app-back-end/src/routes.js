cdb0dacec7fcf74dbae28879bc674ff84fd1e328
vendraaaAjaaa <vendrafausta88@gmail.com>
2025-12-30 01:59:14 +0700

    add feature delete, edit notes

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,5 +1,11 @@
 const { method } = require('lodash');
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -16,6 +22,16 @@ const routes = [
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 ];
 
